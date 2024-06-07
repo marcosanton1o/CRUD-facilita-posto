38dd4eb5ad41bace2e033f5592279ed397510def
Marcos Antônio <151541961+marcosanton1o@users.noreply.github.com>
2024-06-06 21:55:11 -0300

    FacilitaPosto

--- FacilitaPosto/código/login.php
+++ FacilitaPosto/código/login.php
@@ -13,7 +13,7 @@
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
         <style>
         body {
-          background-color: white;
+          background-color: #FFD700;
           color: black;
           height: 100%;
         }
@@ -28,15 +28,15 @@
           font-family: 'Roboto', sans-serif;
         }
         #form {
-          background-color: #FFD700;
+          background-color: white;
           width: 100%;
         }
         #mostrarSenha{
           font-size:10px;
         }
         #botao{
-            font-size: 20px; /* Altere o valor conforme necessário */
-            padding: 30px 20px;
+            font-size: 20px;
+            padding: 10px;
         }
       </style>
 </head>
@@ -44,7 +44,7 @@
     <main class="form-container">
       <form action="verify/logar.php" method="POST" class=" p-5 rounded shadow" id="form">
           <div class="mb-3 mt-3 d-flex justify-content-center align-items-center flex-wrap">
-            <img src="imagens/taxilogo.jpg" alt="" width="60px" heigth="60px">
+            <img src="../imagens/taxilogo.jpg" alt="" width="60px" heigth="60px">
             <h2>FacilitaPosto</h2>
           </div>
           <div class="mb-3 mt-3 py-2">
@@ -55,15 +55,14 @@
             <label for="inputSenha" class="form-label">Senha:</label>
             <input type="password" id="inputSenha" minlength="8" name="senha" class="form-control" required>
           </div>
-          <div class="d-flex justify-content-between align-items-center">
+          
             <div class="form-check mt-3 mb-3">
               <input type="checkbox" class="form-check-input" id="mostrarSenha" onclick="mostrarsenha()">
               <label class="form-check-label " for="mostrarSenha">Mostrar senha</label>
             </div>
-            <a href="login2.php" class="mt-3 mb-3">Entrar como Adm</a>
-          </div>
-          <div class="d-flex justify-content-center mt-4 ">
-            <input class="btn btn-primary w-50 p-3 h1" type="submit" name="botao" id="botao" value="Login"></input>
+            <a href="login2.php" class="mt-3 mb-3 d-flex justify-content-center">Entrar como Adm</a>
+          <div class="d-flex justify-content-center mt-4">
+            <input class="btn btn-primary mx-1 px-5" type="submit" name="botao" id="botao" value="Login">
           </div>
       </form>
     </main>
