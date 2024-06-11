7221b786b4eaadf39c9da4301dfb502b1e742e29
Marcos Antônio <151541961+marcosanton1o@users.noreply.github.com>
2024-06-10 22:06:11 -0300

    ok

--- FacilitaPosto/código/taxistas/taxista/elementos/header.php
+++ FacilitaPosto/código/taxistas/taxista/elementos/header.php
@@ -32,11 +32,14 @@
   </div>
   <div class="offcanvas offcanvas-start" tabindex="-1" id="navbarOffcanvasLg" aria-labelledby="navbarOffcanvasLgLabel">
       <div class="offcanvas-header">
-      
+      <div class="navbar-brand mt-1 d-flex align-items-center" href="#">
+    <img id="logo" src="imagens/taxilogo.jpg" alt="" width="50px" heigth="50px">
+    <h4>Facilita Posto</h4>
+  </div>
         <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body align-items-center h4">
-        <ul class="navbar-nav justify-content-end  mt-2 flex-grow-1 pe-3">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item px-4 py-2">
             <a aria-current="page" href="index.php">Página Inicial</a>
           </li>
--- FacilitaPosto/código/taxistas/taxista/index.php
+++ FacilitaPosto/código/taxistas/taxista/index.php
@@ -1,9 +1,21 @@
 <?php
 include('elementos/comando.php')
 ?>
+
 <?php
 include('elementos/header.php');
 ?>
+<main>
+<?php
+$id_posto_usuario = ":posto_id_posto"
+
+$sqlcon = "SELECT COUNT(*) AS total_taxistas FROM posto WHERE membro.posto_id_posto = :id_posto";
+$resu = $conn->prepare($sqlcon);
+$resu->bindParam(':id_posto', $posto_id_usuario, PDO::PARAM_INT);
+$resu->execute();
+$taxistas = $resu->fetch(PDO::FETCH_ASSOC);
+?>
+</main>
 <?php
 include('elementos/footer.php');
 ?>
