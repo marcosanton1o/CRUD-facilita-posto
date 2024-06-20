8991287d90cf19d5752135f9b44841900c4af990
Marcos Antônio <151541961+marcosanton1o@users.noreply.github.com>
2024-06-19 21:14:27 -0300

    ok

--- FacilitaPosto/código/taxistas/adm/verify/cadastrar.php
+++ FacilitaPosto/código/taxistas/adm/verify/cadastrar.php
@@ -4,10 +4,11 @@ require('../../conexao.php');
 
 if (!isset($_SESSION['id_taxista'])) {
     header('Location: ../login.php');
-}
+    exit;
+} else {
 
 if (isset($_POST['botao']) && !empty($_POST['botao'])) {
-    if (isset($_POST['nome'], $_POST['senha'], $_POST['numeroTel'], $_POST['email'], $_POST['placa_carro'], $_POST['cpf'], $_POST['idade'])&& !empty($_POST['nome'], $_POST['senha'], $_POST['numeroTel'], $_POST['email'], $_POST['placa_carro'], $_POST['cpf'], $_POST['idade'])) {
+    if (isset($_POST['nome'], $_POST['senha'], $_POST['numeroTel'], $_POST['email'], $_POST['placa_carro'], $_POST['cpf'], $_POST['idade']) && !empty($_POST['nome']) && !empty($_POST['senha']) && !empty($_POST['numeroTel']) && !empty($_POST['email']) && !empty($_POST['placa_carro']) && !empty($_POST['cpf']) && !empty($_POST['idade'])) {
 
         $nome = $_POST['nome'];
         $senha = $_POST['senha'];
@@ -18,38 +19,39 @@ if (isset($_POST['botao']) && !empty($_POST['botao'])) {
         $idade = $_POST['idade'];
 
         $id_taxista = $_SESSION['id_taxista'];
-        $sql_posto = "SELECT membro.posto_id_posto FROM membro WHERE id_taxista = :id_taxista";
+        
+        $sql_posto = "SELECT posto_id_posto FROM membro WHERE id_taxista = :id_taxista";
         $resupos = $conn->prepare($sql_posto);
         $resupos->bindValue(':id_taxista', $id_taxista);
         $resupos->execute();
-        $resuposto = $resuposto->fetch(PDO::FETCH_ASSOC);
-        if ($resulposto) {
-            $posto_id_posto = $resultado['posto_id_posto'];
+        $resuposto = $resupos->fetch(PDO::FETCH_ASSOC);
 
-                $sql = "INSERT INTO membro(nome, senha, numeroTel, email, placa_carro, cpf, idade, posto_id_posto) 
-                        VALUES(:nome, :senha, :numeroTel, :email, :placa_carro, :cpf, :idade, :posto_id_posto)";
-                $resu = $conn->prepare($sql);
+        if ($resuposto) {
+            $posto_id_posto = $resuposto['posto_id_posto'];
 
-                $resu->bindValue(":nome", $nome);
-                $resu->bindValue(":senha", $senha);
-                $resu->bindValue(":numeroTel", $numeroTel);
-                $resu->bindValue(":email", $email);
-                $resu->bindValue(":placa_carro", $placa_carro);
-                $resu->bindValue(":cpf", $cpf);
-                $resu->bindValue(":idade", $idade);
-                $resu->bindValue(":posto_id_posto", $posto_id_posto);
-                $resu->execute();
-if ($resu->rowCount() > 0) {
-    header("Location: ../posto.php")
-}
-                header('Location: ../posto.php');
-                exit; 
+            $sql = "INSERT INTO membro (nome, senha, numeroTel, email, placa_carro, cpf, idade, posto_id_posto) 
+                    VALUES (:nome, :senha, :numeroTel, :email, :placa_carro, :cpf, :idade, :posto_id_posto)";
+            $resu = $conn->prepare($sql);
+
+            $resu->bindValue(":nome", $nome);
+            $resu->bindValue(":senha", $senha);
+            $resu->bindValue(":numeroTel", $numeroTel);
+            $resu->bindValue(":email", $email);
+            $resu->bindValue(":placa_carro", $placa_carro);
+            $resu->bindValue(":cpf", $cpf);
+            $resu->bindValue(":idade", $idade);
+            $resu->bindValue(":posto_id_posto", $posto_id_posto);
+            $resu->execute();
+
+            header('Location: ../posto.php');
+            exit;
         } else {
-            echo "algo está errado com o formulário";
+            echo "algum erro aconteceu, tente novamente";
         }
     } else {
         header('Location: ../posto.php');
         exit;
     }
+}
 }
 ?>
